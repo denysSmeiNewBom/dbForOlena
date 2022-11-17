87535ba8fcf2005c83150bd660acdcbfbec51895
Denys Smei <denys.smei@artcodevision.com>
2022-11-17 17:58:36 +0200

    Completed template

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,9 @@
-public class Main {
+import service.UserService;
+import service.UserServiceImpl;
 
+public class Main {
+    public static void main(String[] args) {
+        UserService service = new UserServiceImpl();
+        //do all service;
+    }
 }
